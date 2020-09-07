57e1de8ef5692899c76ff50671b1d57ce8f64c43
Carlos Navarro <elgyro99@gmail.com>
2020-09-07 13:12:54 -0500

    Added Splash/Main menu

---
+++ Halo-defenders/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            LoadMainMenu();
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine(SplashSceneDelay());
+
+    }
+    IEnumerator SplashSceneDelay()
+    {
+
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("MainMenu");
+
+    }
+}
